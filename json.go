7a8716d82c98760eb385b2520b09422b04fd3af6
syumai <syumai@gmail.com>
2021-07-31 18:48:16 +0900

    Add random select option

--- json.go
+++ json.go
@@ -1,37 +1,19 @@
-//go:generate statik -src=./assets
 package emojidata
 
 import (
-	"io"
-	"io/ioutil"
+	_ "embed"
 
 	"github.com/goccy/go-json"
-	"github.com/rakyll/statik/fs"
-	_ "github.com/syumai/emojidata/statik"
 )
+
+//go:embed assets/emoji-data/emoji_pretty.json
+var emojiDataJSON []byte
 
 func unmarshalEmojiDataJSON() ([]*Emoji, error) {
 	var emojis []*Emoji
-	emojiDataFile, err := openDataFile()
-	defer emojiDataFile.Close()
-	if err != nil {
-		return nil, err
-	}
-	blob, err := ioutil.ReadAll(emojiDataFile)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(blob, &emojis)
+	err := json.Unmarshal(emojiDataJSON, &emojis)
 	if err != nil {
 		return nil, err
 	}
 	return emojis, err
-}
-
-func openDataFile() (io.ReadCloser, error) {
-	staticFS, err := fs.New()
-	if err != nil {
-		return nil, err
-	}
-	return staticFS.Open("/emoji-data/emoji_pretty.json")
 }
